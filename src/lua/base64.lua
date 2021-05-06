5f8f7d67ce208e32d4f679571be1e50314581b6d
xhl <heramerom@gmail.com>
2021-05-06 22:29:44 +0800

    init

---
+++ src/lua/base64.lua
@@ -0,0 +1,29 @@
+-- @begin
+-- @title Base64 Encode
+-- @desc Base 64 encode and decode
+-- @author heramerom
+-- @homepage 
+-- @type action
+-- @menu Copy With Base64 Encode
+-- @menu Copy With Base64 Decode
+-- @menu Replace With Base64 Encode
+-- @menu Replace With Base64 Decode
+-- @require https://raw.githubusercontent.com/toastdriven/lua-base64/master/base64.lua
+-- @end
+
+
+function base64Encode(data)
+
+end
+
+
+function base64Decode(data)
+
+end
+
+
+if action.menu == 'Copy With Base64 Encode' then
+
+
+end
+
---
+++ src/lua/objectId.lua
@@ -0,0 +1,16 @@
+--
+-- @title Bson Object ID
+-- @desc  Bson Object ID convert time
+-- @type  action
+-- @menu  Copy Datetime From Bson ID
+-- 
+-------
+
+if action.menu = 'Copy Datetime From Bson ID' then
+
+    
+    
+
+    
+end
+
