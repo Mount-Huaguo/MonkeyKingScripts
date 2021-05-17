3d5a0ad245190ac9727e55a786eb28a7374b687f
xhl <heramerom@gmail.com>
2021-05-17 18:42:51 +0800

    test

---
+++ src/lua/Base64/intro.lua
@@ -0,0 +1,16 @@
+
+return {
+    author = 'Heramerom',
+    homepage = 'https://github.com/heramerom',
+    description = [[
+        <html>
+            <body>
+            Base64 encode and decode methods suits.
+            </body>
+        </html>
+    ]],
+    capture = {
+        -- 1 = "",
+        -- 2 = ""
+    }   
+}
---
+++ src/lua/ObjectID/intro.lua
@@ -0,0 +1,16 @@
+
+return {
+    author = 'Heramerom',
+    homepage = 'https://github.com/heramerom',
+    description = [[
+        <html>
+            <body>
+            Bson object id encode and decode methods suit
+            </body>
+        </html>
+    ]],
+    capture = {
+        -- 1 = "",
+        -- 2 = ""
+    }   
+}
