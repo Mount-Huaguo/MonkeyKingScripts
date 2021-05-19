a34349e6153a400e8cfd59d1a8e0ec61d7de219e
xhl <heramerom@gmail.com>
2021-05-19 15:28:30 +0800

    test

--- index.lua
+++ index.lua
@@ -8,13 +8,19 @@ return {
     {
         name = 'Base64 Encode And Decode', 
         language = 'lua',
-        intro = 'src/lua/Base64/intro.lua', 
-        source = 'src/lua/Base64/base64.lua',
+        intro = '/src/lua/Base64/intro.html', 
+        source = '/src/lua/Base64/base64.lua',
     },
     {
         name = 'Bson ObjectID Convert', 
         language = 'lua',
-        intro = 'src/lua/ObjectID/intro.lua', 
-        source = 'src/lua/ObjectID/id.lua',
+        intro = '/src/lua/ObjectID/intro.html', 
+        source = '/src/lua/ObjectID/id.lua',
     },
+    {
+        name = 'Hex Encode And Decode',
+        language = 'js'
+        intro = '/src/js/Hex/hex.html',
+        source = '/src/js/Hex/hex.js'
+    }
 }
