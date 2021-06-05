4b65afd4f9ec00701828eb724849da88d5048a75
xhl <heramerom@gmail.com>
2021-06-05 23:19:25 +0800

    Update hex methods

--- src/js/Hex/hex.js
+++ src/js/Hex/hex.js
@@ -1,23 +1,48 @@
 // @start
 // @namespace com.github.mounthuaguo.mongkeyking
 // @version 0.1
-// @name Hex encode and decode
+// @name Hexadecimal encode and decode
 // @type action
-// @action Hex Encode
-// @action Hex Decode
+// @action Decimal To Hexadecimal
+// @action Hexadecimal To Decimal
+// @action Encode String To Hexadecimal
+// @action Decode Hexadecimal To String
 // @end 
 
 
 (function(){
-    
-    if (menu === 'Hex Encode') {
-        // todo
-        return;
+    var Integer = Java.type('java.lang.Integer')
+    var String = Java.type('java.lang.String')
+    var Hex = Java.type('org.apache.commons.codec.binary.Hex')
+
+    var input = event.selectionModel().selectedText()
+    if (!input) {
+        toast.warn('Select text is needed.')
+        return 
+    }
+    var start = event.selectionModel().selectionStart()
+    var end = event.selectionModel().selectionEnd()
+
+    if (menu === 'Decimal To Hexadecimal') {
+        var n = Integer.parseInt(input)
+        event.document().replaceString(start, end, Integer.toHexString(n))
     }
 
-    if (menu === 'Hex Decode') {
-        // todo
-        return;
+    if (menu === 'Hexadecimal To Decimal') {
+        var n = Integer.parseInt(input, 16)
+        event.document().replaceString(start, end, Integer.toString())
+    }
+
+    if (menu === 'Encode String To Hexadecimal') {
+        var s = new String(input)
+        var hexStr = Hex.encodeHexString(s.getBytes())
+        event.document().replaceString(start, end, hexStr)
+    }
+
+    if (menu === 'Decode Hexadecimal To String') {
+        var hexStr = new String(input)
+        var s = Hex.decodeHex(s).toString()
+        event.document().replaceString(start, end, s)
     }
 
 })()
